fb3dfc55854ae0c9dc1bc278a4fdde4eb6daf3f7
varaidzo-mawere <varaidzo.mawere@uncommon.org>
2022-01-21 11:10:01 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,102 @@
+
+
+window.onload=function(){
+    const menu_btn=document.querySelector('.hamburger');
+    const mobile_menu= document.querySelector('.mobile-nav');
+
+    menu_btn.addEventListener('click', function(){
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    })
+   
+}
+
+document.getElementById("Christmas-party").style.display ='block';
+document.getElementById("eat-event").style.display ='none';
+document.getElementById("Fundraiser").style.display ='none';
+document.getElementById("Winter-warmup").style.display ='none';
+
+
+
+
+
+const myFunctionA = () => {
+    document.getElementById("Christmas-party").style.display ='block';
+    document.getElementById("eat-event").style.display ='none';
+    document.getElementById("Fundraiser").style.display ='none';
+    document.getElementById("Winter-warmup").style.display ='none';
+    document.getElementById("button4").style.display ='none';
+  
+}
+
+
+const myFunctionB = () => {
+    document.getElementById("Christmas-party").style.display ='none';
+    document.getElementById("eat-event").style.display ='block';
+    document.getElementById("Fundraiser").style.display ='none';
+    document.getElementById("Winter-warmup").style.display ='none';
+}
+
+
+const myFunctionC = () => {
+document.getElementById("Christmas-party").style.display ='none';
+document.getElementById("eat-event").style.display ='none';
+document.getElementById("Fundraiser").style.display ='block';
+document.getElementById("Winter-warmup").style.display ='none';
+}
+
+const myFunctionD = () => {
+    document.getElementById("Christmas-party").style.display ='none';
+    document.getElementById("eat-event").style.display ='none';
+    document.getElementById("Fundraiser").style.display ='none';
+    document.getElementById("Winter-warmup").style.display ='block';
+    }
+    
+
+
+
+
+
+
+document.getElementById("button1").style.display ='block';
+document.getElementById("button2").style.display ='none';
+document.getElementById("button3").style.display ='none';
+document.getElementById("button4").style.display ='none';
+
+
+
+
+const myFunction1 = () => {
+    document.getElementById("button1").style.display ='block';
+   
+    document.getElementById("button2").style.display ='none';
+    document.getElementById("button3").style.display ='none';
+    document.getElementById("button4").style.display ='none';
+  
+}
+
+
+const myFunction2 = () => {
+    document.getElementById("button1").style.display ='none';
+    document.getElementById("button2").style.display ='block';
+    document.getElementById("button3").style.display ='none';
+    document.getElementById("button4").style.display ='none';
+}
+
+
+const myFunction3 = () => {
+    document.getElementById("button1").style.display ='none';
+    document.getElementById("button2").style.display ='none';
+    document.getElementById("button3").style.display ='block';
+    document.getElementById("button4").style.display ='none';
+}
+
+
+const myFunction4 = () => {
+    document.getElementById("button1").style.display ='none';
+    document.getElementById("button2").style.display ='none';
+    document.getElementById("button3").style.display ='none';
+    document.getElementById("button4").style.display ='block';
+}
+
+
---
+++ testing.js
@@ -0,0 +1,9 @@
+$('.campaign').slick({
+  infinite: true,
+	dots: true,
+	arrows: false,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	fade: true,
+	fadeSpeed: 1000
+});
